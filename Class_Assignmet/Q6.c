c66c8171788b6c899ed1955475af32fef5a43ff4
RAJYADAV666 <rajyadav968940@gmail.com>
2024-10-06 00:58:24 +0530

    Add files via upload

---
+++ Class_Assignmet/Q6.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+
+        int i=0;
+        float f=0.0;
+        double d=0;
+
+         printf(" The size of int is %d Byte\n ",sizeof(int));
+         printf(" The size of float is %d Byte\n",sizeof(float));
+         printf(" The size of double is %d Byte\n",sizeof(double));
+
+
+
+
+}
