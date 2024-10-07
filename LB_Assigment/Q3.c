816a69f5e7854fa7655982e4899965886bf4562a
RAJYADAV666 <rajyadav968940@gmail.com>
2024-10-07 22:50:20 +0530

    Add files via upload

---
+++ LB_Assigment/Q3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void Display()
+{
+        int iCnt=0;
+
+        for(iCnt=5; iCnt>=1 ; iCnt--)
+        {
+                printf("%d\n",iCnt);
+        }
+
+
+
+}
+
+int main()
+{
+        int iNo=0;
+        
+       
+
+        Display(iNo);
+
+
+    return 0;
+}
