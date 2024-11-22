78db263a8caa3f068dc22fb3347e1bd01399f5a6
RAJYADAV666 <rajyadav968940@gmail.com>
2024-11-22 23:16:24 +0530

    Add files via upload

---
+++ LB_Assignment/Assignment _18/Q3.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+///////////////////////////////////////
+//
+//      Function Name:Pattern
+//      Description:To display the Pattern
+//      Input Argument:Integer 
+//      Output:-
+//      Date:23/10/2024
+//      Author:Raj Kishor Yadav
+///////////////////////////////////////
+
+void Pattern(int iNo1,int iNo2)
+{
+        int i=0;
+        int j=0;
+
+        for(i=iNo1;i>=1;i--)
+        {
+            for(j=iNo2;j>=1;j--)
+            {
+                    if(j %2==1)
+                    {
+                        printf("%d\t",j);
+                    }
+                    else
+                    {
+                        printf("%d\t",j);
+                    }
+               
+            }
+
+                  printf("\n");
+        }
+            
+
+
+
+}
+int main()
+{
+
+
+int iValue1=0;
+int iValue2=0;
+
+printf("Enter the Row Size:\n");
+scanf("%d",&iValue1);
+
+printf("Enter the Coloumn Size:\n");
+scanf("%d",&iValue2);
+
+Pattern(iValue1,iValue2);
+
+    return 0;
+}
---
+++ LB_Assignment/Assignment _18/Q4.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+///////////////////////////////////////
+//
+//      Function Name:Pattern
+//      Description:To display the Pattern
+//      Input Argument:Integer 
+//      Output:-
+//      Date:23/10/2024
+//      Author:Raj Kishor Yadav
+///////////////////////////////////////
+
+void Pattern(int iNo1,int iNo2)
+{
+        int i=0;
+        int j=0;
+
+        for(i=1;i<=iNo1;i++)
+        {
+            for(j=1;j<=iNo2;j++)
+            {
+                    if(j %2 ==1)
+                    {
+                        printf("*\t");
+                    }
+                    else
+                    {
+                        printf("#\t");
+                    }
+               
+            }
+
+                  printf("\n");
+        }
+            
+
+
+
+}
+int main()
+{
+
+
+int iValue1=0;
+int iValue2=0;
+
+printf("Enter the Row Size:\n");
+scanf("%d",&iValue1);
+
+printf("Enter the Coloumn Size:\n");
+scanf("%d",&iValue2);
+
+Pattern(iValue1,iValue2);
+
+    return 0;
+}
