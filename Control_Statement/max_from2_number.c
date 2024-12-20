2bdd4970c0b16a7a388e60425b3206f2bb14f5c2
RAJYADAV666 <rajyadav968940@gmail.com>
2024-12-20 11:06:14 +0530

    Add files via upload

---
+++ Control_Statement/max_from2_number.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+    int no1=0;
+    int no2=0;
+
+    up:
+
+    printf(" enter a first number \n");
+    scanf("%d",&no1);
+
+    printf(" enter a second number\n");
+    scanf("%d",&no2);
+
+    if(no1==no2)
+    {
+        printf(" both given number are the same ");
+       
+    }
+
+     else if(no1>no2)
+    {
+        printf(" number 1 %d is maximum",no1);
+    }
+    else
+    {
+        printf("number 2 %d is maximum",no2);
+
+    }
+    return 0;
+}
