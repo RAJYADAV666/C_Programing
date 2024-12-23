9d7d1b61b4f42cb507cc6827580160ae89536ad4
RAJYADAV666 <rajyadav968940@gmail.com>
2024-12-24 01:59:00 +0530

    Add files via upload

---
+++ Array/Assignment _Array/Q9.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int Count(int *Arr,int iSize)
+{
+
+    int iCnt=0;
+    int i=0;
+
+    for(iCnt=0;iCnt<iSize;iCnt++)
+    {
+        i++;
+
+    }
+
+    return i;
+
+
+}
+int main()
+{
+    int iCnt=0;
+    int iValue=0;
+    int *Ptr=NULL;
+    int iCount=0;
+
+
+    printf("Enter The Number of Element You Want:\n");
+    scanf("%d",&iValue);
+
+    Ptr=(int*)malloc(iValue*sizeof(int));
+
+    printf("Dynamic Memorey Allocated Succefully:\n");
+
+    for(iCnt=0;iCnt<iValue;iCnt++)
+    {
+        printf("Enter %d Elemnt:\n",iCnt+1);
+        scanf("%d",&Ptr[iCnt]);
+
+
+    }
+
+    iCount=Count(Ptr,iValue);
+
+    printf("The Total Number of Elment is %d\n",iCount);
+
+
+
+
+
+
+
+    return 0;
+}
