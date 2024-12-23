e61dd771bff151df17f59cff6e7affcd64c8b196
RAJYADAV666 <rajyadav968940@gmail.com>
2024-12-24 01:58:23 +0530

    Add files via upload

---
+++ Array/Assignment _Array/Q5.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main()
+{
+    int Arr[7]={0};
+    int i=0;
+
+    for(i=0;i<7;i++)
+    {
+        printf("Enter the %d Element:\n",i+1);
+        scanf("%d",&Arr[i]);
+
+    }
+
+    for(i=0;i<7;i++)
+    {
+        printf("Array Element is %d\n",Arr[i]);
+        
+
+    }
+
+
+
+
+
+
+
+
+    return 0;
+}
