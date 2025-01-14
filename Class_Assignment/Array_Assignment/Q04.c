40a104fce1b17361bddbb657bd341d688487a5bd
Raj Yadav <rajyadav968940@gmail.com>
2025-01-15 01:20:29 +0530

    Add files via upload

---
+++ Class_Assignment/Array_Assignment/Q04.c
@@ -0,0 +1,21 @@
+
+
+#include<stdio.h>
+
+int main()
+{
+
+    int Arr[5]={10,20,30,40,50};
+
+ printf(" Array First Element is %d\n",Arr[0]);
+ printf(" Array Second  Element is %d\n",Arr[1]);
+ printf(" Array Third Element is %d\n",Arr[2]);
+ printf(" Array Fourth Element is %d\n",Arr[3]);
+ printf(" Array Fifth Element is %d\n",Arr[4]);
+
+ return 0;
+
+
+
+}
+
---
+++ Class_Assignment/Array_Assignment/Q10.c
@@ -0,0 +1,55 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+int Max(int *Arr,int iSize)
+{
+    int i=0;
+    int iMax=0;
+
+    iMax=Arr[0];
+
+    for(i=0;i<iSize;i++)
+    {
+        if((Arr[i])>iMax)
+        {
+            iMax=Arr[i];
+        }
+
+    }
+return iMax;
+
+
+}
+
+int main()
+{
+    int iCount=0;
+    int iCnt=0;
+    int *Ptr=NULL;
+    int iAns=0;
+
+    printf("Enter The Number of Element you Want:\n");
+    scanf("%d",&iCount);
+
+    Ptr=(int*)malloc(iCount*sizeof(int));
+
+    printf("Dyanmic Memorey Get Allocateted Succesfully:\n");
+
+    printf("Enter the %d Element:",iCount);
+
+    for(iCnt=0;iCnt<iCount;iCnt++)
+    {
+        printf("Enter the %d Element:\n",iCnt+1);
+        scanf("%d",&Ptr[iCnt]);
+    }
+
+    iAns=Max(Ptr,iCount);
+
+    printf("The Maximum Element is %d\n",iAns);
+
+
+
+
+    return 0;
+}
+
