8aef22bbdadade3c82565aa21f6ef76f08d43bf9
Raj Yadav <rajyadav968940@gmail.com>
2025-01-15 00:09:49 +0530

    Add files via upload

---
+++ Selection/Q08.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+void Display(int iValue)
+{
+    if(iValue==1)
+    {
+        printf("Sunday:\n");
+    }
+    else if(iValue==2)
+    {
+        printf("Monday:\n");
+    }
+     else if(iValue==3)
+    {
+        printf("Tuesday:\n");
+    }
+     else if(iValue==4)
+    {
+        printf("Wednesday:\n");
+    }
+     else if(iValue==5)
+    {
+        printf("Thursday\n");
+    }
+     else if(iValue==6)
+    {
+        printf("Friday:\n");
+    }
+    else if(iValue==7)
+    {
+        printf("Saturday:\n");
+    }
+    else
+    {
+        printf("Invalid Position:\n");
+    }
+
+
+}
+int main()
+{
+
+
+    int iNo=0;
+
+    printf("Enter the Number:\n");
+    scanf("%d",&iNo);
+
+    Display(iNo);
+    return 0;
+
+}
+
